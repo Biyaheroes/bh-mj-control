d4a3a222af9b8ef819b4a4aa0107da55daa82f5c
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-08-03 06:53:24 +0800

    Update bh-mj-control component.

---
+++ component.jsx
@@ -0,0 +1,16 @@
+
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Control from "./control.js";
+
+ReactDOM.render( 
+		<Control
+			label="This is a control">
+		
+		</Control>,
+	
+	document.getElementById( "root" )
+ );
--- test.jsx
+++
@@ -1,9 +0,0 @@
-
-"use strict";
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Control from "./control.js";
-
-ReactDOM.render( <Control></Control> );
